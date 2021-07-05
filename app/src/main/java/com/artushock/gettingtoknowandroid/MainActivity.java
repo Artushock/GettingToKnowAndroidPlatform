4ef149b89a9f9251d64c0fe2b69def174882786c
Artiom Ponomariov <artushock@gmail.com>
2021-07-05 16:12:33 +0300

    Added third task - CalendarView.

--- app/src/main/java/com/artushock/gettingtoknowandroid/MainActivity.java
+++ app/src/main/java/com/artushock/gettingtoknowandroid/MainActivity.java
@@ -1,12 +1,14 @@
 package com.artushock.gettingtoknowandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CompoundButton;
+import android.widget.CalendarView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.test_linear_layout);
-        setContentView(R.layout.test_edit_text_layout);
+//        setContentView(R.layout.test_edit_text_layout);
+        setContentView(R.layout.test_calendar_layout);
 
         ToggleButton toggleButton = findViewById(R.id.remember_button);
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendar_view);
+
 
 //        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 //            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -27,12 +32,18 @@ public class MainActivity extends AppCompatActivity {
 //                }
 //            }
 //        });
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = String.format("Выбрана дата: %d.%d.%d", dayOfMonth, month, year);
+                Toast.makeText(view.getContext(), date, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void onClick(View view) {
         Toast toast = Toast.makeText(MainActivity.this, "Данные отправлены", Toast.LENGTH_SHORT);
         toast.show();
     }
-
-
 }
